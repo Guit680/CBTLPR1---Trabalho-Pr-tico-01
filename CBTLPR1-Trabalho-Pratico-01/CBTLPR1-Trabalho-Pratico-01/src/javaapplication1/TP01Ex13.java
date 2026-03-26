b2f83139dec8dc043b22226f0b748dbf18cc4a46
Guit680 <guilhermet17012005@gmail.com>
2026-03-25 21:45:31 -0300

    Update TP01Ex13.java
    Added comments explaining the purpose of the program.

--- CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex13.java
+++ CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex13.java
@@ -1,6 +1,12 @@
 package javaapplication1;
 import java.util.Scanner;
 
+/*13. Calcular e exibir a velocidade final (em km/h) de um automóvel, a partir dos
+valores da velocidade inicial (em m/s), da aceleração (m/s2) e do tempo de percurso
+(em s) que serão digitados.
+Guilherme Postigo
+Izadora Santos*/
+
 public class TP01Ex13 {
 	public static void main (String [] args){
 	    Scanner scanner = new Scanner (System.in);
