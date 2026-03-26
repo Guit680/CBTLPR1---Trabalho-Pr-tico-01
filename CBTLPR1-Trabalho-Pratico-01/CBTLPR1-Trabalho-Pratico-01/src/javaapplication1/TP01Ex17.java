af3057daca3bcb8824251ca05dc3356f6ac76f5a
Guit680 <guilhermet17012005@gmail.com>
2026-03-25 21:47:28 -0300

    Update TP01Ex17.java
    Updated comments and added a description for the program.

--- CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex17.java
+++ CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex17.java
@@ -1,6 +1,11 @@
 package javaapplication1;
 import java.util.Scanner;
 
+/*17. Entrar via teclado com dois valores quaisquer “X” e “Y”. Calcular e exibir o
+cálculo XY (“X” elevado a “Y”). Pesquisar as funções Exp e Ln.
+Guilherme Postigo
+Izadora Santos*/
+
 public class TP01Ex17 {
 	public static void main (String [] args){
 	Scanner scanner = new Scanner(System.in);
