a693dce3e7742c51333c6731534a34fe20f5684b
Guit680 <guilhermet17012005@gmail.com>
2026-03-25 21:46:57 -0300

    Update TP01Ex16.java
    Added comments to explain the purpose of the program.

--- CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex16.java
+++ CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex16.java
@@ -1,6 +1,12 @@
 package javaapplication1;
 import java.util.Scanner;
 
+/*16. Entrar via teclado com o valor de um ângulo em graus, calcular e exibir as
+seguintes funções trigonométricas: seno, cosseno, tangente e secante deste ângulo.
+Lembre-se que uma função trigonométrica trabalha em radianos.
+Guilherme Postigo
+Izadora Santos*/
+
 public class TP01Ex16 {
 	public static void main (String [] args){
 		Scanner scanner = new Scanner(System.in);
