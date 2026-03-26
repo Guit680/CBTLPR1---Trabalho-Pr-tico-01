ada4c6d036cda5cf5579f377cef956b45c7a1d1b
Guit680 <guilhermet17012005@gmail.com>
2026-03-25 21:41:56 -0300

    Update TP01Ex08.java
    Added comments explaining the purpose of the program.

--- CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex08.java
+++ CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex08.java
@@ -1,6 +1,12 @@
 package javaapplication1;
 import java.util.Scanner;
 
+/*8. Sabendo que uma milha marítima equivale a um mil, oitocentos e cinquenta e
+dois metros e que um quilômetro possui mil metros, fazer um programa para
+converter milhas marítimas em quilômetros.
+Guilherme Postigo
+Izadora Santos*/
+
 public class TP01Ex08 {
 	public static void main (String [] args){
 	    Scanner scanner = new Scanner (System.in);
