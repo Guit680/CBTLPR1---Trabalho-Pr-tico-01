c8773381873fbd37d55c4d4b1fb6f9b42ddd33db
Guit680 <guilhermet17012005@gmail.com>
2026-03-25 21:42:34 -0300

    Update TP01Ex09.java
    Added comments to explain the purpose of the program.

--- CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex09.java
+++ CBTLPR1-Trabalho-Pratico-01/CBTLPR1-Trabalho-Pratico-01/src/javaapplication1/TP01Ex09.java
@@ -1,6 +1,11 @@
 package javaapplication1;
 import java.util.Scanner;
 
+/*9. Calcular e exibir a tensão de um determinado circuito eletrônico a partir dos
+valores da resistência e corrente elétrica que serão digitados. Utilize a lei de Ohm.
+Guilherme Postigo
+Izadora Santos*/
+
 public class TP01Ex09 {
 	public static void main (String [] args){
 	    Scanner scanner = new Scanner (System.in);
